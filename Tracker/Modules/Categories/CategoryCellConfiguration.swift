61b2786dd5394633945305c06ce62b07e9f05675
Svetlana <sveise@mail.ru>
2025-10-30 21:15:47 +0100

    Изменена структура проекта

---
+++ Tracker/Modules/Categories/CategoryCellConfiguration.swift
@@ -0,0 +1,16 @@
+//
+//  CategoryCellConfiguration.swift
+//  Tracker
+//
+//  Created by Svetlana Varenova on 30.10.2025.
+//
+
+import UIKit
+
+struct CategoryCellConfiguration {
+    let title: String
+    let isSelected: Bool
+    let isFirst: Bool
+    let isLast: Bool
+    let showSeparator: Bool
+}
